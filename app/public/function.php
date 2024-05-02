fae02fd9d5d2abb2137481a748f4208ce3b2bb0b
kingchobo <kingchobo2022@gmail.com>
2024-05-03 08:06:32 +0900

    24.5.3 write_ok.php insert into

--- app/public/function.php
+++ app/public/function.php
@@ -9,3 +9,9 @@ function makeFileName($file) {
     // 240503111111
     return date('ymdHis') .'_'. rand(1000, 9999) .'.'. $ext;
 }
+
+// $name = (isset($_POST['name']) && $_POST['name'] != '') ? $_POST['name'] : '';
+
+function getPost($var) {
+    return (isset($_POST[$var]) && $_POST[$var] != '') ? $_POST[$var] : '';
+}
--- app/public/write_ok.php
+++ app/public/write_ok.php
@@ -2,10 +2,55 @@
 require "connect.php";
 require "function.php";
 
+$name = getPost('name');
+$passwd = getPost('passwd');
+$subject = getPost('subject');
+$content = getPost('content');
+
+if ($name == '') {
+    exit('이름이 비어 있습니다. <a href="write.php">글쓰기로 이동</a>');
+}
+if ($subject == '') {
+    exit('제목이 비어 있습니다. <a href="write.php">글쓰기로 이동</a>');
+}
+if ($content == '') {
+    exit('글 내용이 비어 있습니다. <a href="write.php">글쓰기로 이동</a>');
+}
+if ($passwd == '') {
+    exit('비밀번호가 비어 있습니다. <a href="write.php">글쓰기로 이동</a>');
+}
+
+
+$filename = '';
 if ( isset($_FILES['file']['tmp_name']) 
     && $_FILES['file']['tmp_name'] != ''
     && is_uploaded_file($_FILES['file']['tmp_name']) ) {
 
     $newfilename = makeFileName($_FILES['file']['name']);
     move_uploaded_file($_FILES['file']['tmp_name'], 'data/'. $newfilename);
+
+    $filename = $newfilename .'|'. $_FILES['file']['name'];
 }
+
+$sql = "INSERT INTO step2 
+    SET name=:name, subject=:subject,
+        passwd=:passwd, content=:content,
+        file=:file, hit=0, rdatetime=NOW()";
+$stmt = $conn->prepare($sql);
+
+$arr = [
+    ':name' => $name,
+    ':passwd' => $passwd,
+    ':subject' => $subject,
+    ':content' => $content,
+    ':file' => $filename
+];
+
+$rs = $stmt->execute($arr);
+
+if ($rs) {
+    exit('글 등록이 성공적으로 이뤄졌습니다. <a href="list.php">글 목록</a>');
+} else {
+    exit('글 등록할때 오류가 발생했습니다. <a href="list.php">글 목록</a>');
+}
+        
