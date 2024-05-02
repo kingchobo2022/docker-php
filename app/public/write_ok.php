18888d9c2164b601f0630a4a7d2adb757dc625f2
kingchobo <kingchobo2022@gmail.com>
2024-05-03 07:22:26 +0900

    24.5.3 write_ok.php file upload

--- app/public/write_ok.php
+++ app/public/write_ok.php
@@ -1,5 +1,15 @@
 <?php
 require "connect.php";
 
+if ( isset($_FILES['file']['tmp_name']) 
+    && $_FILES['file']['tmp_name'] != ''
+    && is_uploaded_file($_FILES['file']['tmp_name']) ) {
+
+    move_uploaded_file($_FILES['file']['tmp_name'], 'data/'. $_FILES['file']['name']);
+
+}
 
 
+print_r($_FILES);
+
+print_r($_POST);
