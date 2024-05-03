abca158e3fb4f0bbabec6985f0d7b606ae032180
kingchobo <kingchobo2022@gmail.com>
2024-05-04 07:22:21 +0900

    24.5.4 list.php paginate

--- app/public/function.php
+++ app/public/function.php
@@ -15,3 +15,30 @@ function makeFileName($file) {
 function getPost($var) {
     return (isset($_POST[$var]) && $_POST[$var] != '') ? $_POST[$var] : '';
 }
+
+function paginate($totalPages, $currentPage, $baseUrl) {
+    $pagination = '';
+
+    // 이전 페이지 링크
+    if ($currentPage > 1) {
+        $prevPage = $currentPage - 1;
+        $pagination .= '<a href="' . $baseUrl . '?page=' . $prevPage . '">이전</a> ';
+    }
+
+    // 페이지 숫자 링크
+    for ($i = 1; $i <= $totalPages; $i++) {
+        if ($i == $currentPage) {
+            $pagination .= ' <span>' . $i . '</span> ';
+        } else {
+            $pagination .= ' <a href="' . $baseUrl . '?page=' . $i . '">' . $i . '</a> ';
+        }
+    }
+
+    // 다음 페이지 링크
+    if ($currentPage < $totalPages) {
+        $nextPage = $currentPage + 1;
+        $pagination .= ' <a href="' . $baseUrl . '?page=' . $nextPage . '">다음</a>';
+    }
+
+    return $pagination;
+}
--- app/public/list.php
+++ app/public/list.php
@@ -1,10 +1,25 @@
 <?php
 require 'connect.php';
+require 'function.php';
+
+
+$currentPage = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
+$baseUrl = 'list.php';
+
+$sql = "SELECT COUNT(*) cnt FROM step2";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$total = $row['cnt']; // total 글 수
+$limit = 5; // 페이지당 글 수
+
+$totalPages = ceil($total / $limit);
+
+$paging = paginate($totalPages, $currentPage, $baseUrl);
 
 $sql = "SELECT idx, name, subject, hit, rdatetime 
     FROM step2 
-    
-    ORDER BY idx DESC LIMIT 5, 5
+    ORDER BY idx DESC LIMIT ".( ($currentPage - 1) * $limit   ).", ".$limit."
     ";
 
 $stmt = $conn->prepare($sql);
@@ -41,5 +56,9 @@ foreach($rs AS $row) {
 }
 ?>
     </table>
+
+<?php
+    echo $paging;
+?>    
 </body>
 </html>
