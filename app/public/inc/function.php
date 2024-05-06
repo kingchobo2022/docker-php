d8fa3ebc65790f40e3f60492b978353d5e27026e
kingchobo <kingchobo2022@gmail.com>
2024-05-07 08:27:01 +0900

    24.5.7 write.php

--- app/public/inc/function.php
+++ app/public/inc/function.php
@@ -7,3 +7,21 @@ function getPost($var) {
 function getGet($var) {
     return (isset($_GET[$var]) && $_GET[$var] != '') ? $_GET[$var] : '';
 }
+
+// $boardNameArr = [
+//     'notice' => '공지사항',
+//     'free' => '자유게시판'
+// ];
+
+
+function getBoardName($code) {
+    global $boardNameArr;
+
+    foreach($boardNameArr AS $key => $value) {
+        if ($code == $key) {
+            return $value;
+        }           
+    }
+
+    return '';
+}
--- app/public/write.php
+++ app/public/write.php
@@ -1,15 +1,10 @@
 <?php
+require 'inc/config.php';
 require 'inc/function.php';
 
 $code = getGet('code');
 
-if($code == 'notice') {
-    $board_title = '공지사항';
-} else if($code == 'free') {
-    $board_title = '자유게시판';
-} else {
-    $board_title = '';
-}
+$board_title = getBoardName($code);
 
 if($board_title == '') {
     exit('올바르지 않은 게시판 코드입니다. <a href="index.php">처음으로</a>');
@@ -26,5 +21,16 @@ if($board_title == '') {
 <body>
     <h1><?= $board_title ?></h1>
     
+    <form method="post" enctype="multipart/form-data" action="write_ok.php" autocomplete="off">
+        <input type="hidden" name="code" value="<?= $code ?>">
+        이름 : <input type="text" name="name" maxlength="30"> <br>
+        비밀번호 : <input type="password" name="passwd" maxlength="50"> <br>
+        제목 : <input type="text" name="subject"> <br>
+        본문 : <br>
+        <textarea cols="100" rows="15" name="content"></textarea> <br>
+        파일첨부<br>
+        <input type="file" name="file"><br>
+        <button>확인</button>
+    </form>
 </body>
 </html>
