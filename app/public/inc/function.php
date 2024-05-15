87a589828fee5aaf2de21cd9fbceb3d84cb1e422
kingchobo <kingchobo2022@gmail.com>
2024-05-15 10:59:43 +0900

    24.5.15 index.php

---
+++ app/public/inc/function.php
@@ -0,0 +1,9 @@
+<?php
+
+function getPost($var) {
+    return (isset($_POST[$var]) && $_POST[$var] != '' ) ? $_POST[$var] : '';
+}
+
+function getGet($var) {
+    return (isset($_GET[$var]) && $_GET[$var] != '' ) ? $_GET[$var] : '';
+}
---
+++ app/public/list.php
@@ -0,0 +1,18 @@
+<?php
+include 'inc/function.php';
+
+session_start();
+
+if (isset($_SESSION['ses_id']) && $_SESSION['ses_id'] != '') {
+    $ses_id = $_SESSION['ses_id'];
+} else {
+    $ses_id = '';
+}
+
+$code = getGet('code');
+
+
+if ($ses_id == '') {
+    exit('로그인 후 이용해 주시기 바랍니다. <a href="login.php?code='.$code.'">로그인 페이지로 이동</a>');
+}
+
