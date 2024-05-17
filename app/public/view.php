717b885b09b33b0f28488b9bdfb0624fc0f5ebde
kingchobo <kingchobo2022@gmail.com>
2024-05-18 08:33:52 +0900

    24.5.18 view.php #2

--- app/public/view.php
+++ app/public/view.php
@@ -27,3 +27,46 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if (!$row) {
     exit('해당 게시물이 존재하지 않습니다.. <a href="list.php?code='.$code.'">'.$board_title.' 목록으로</a>');
 }
+
+$img_tag = '';
+if ($row['file']) {
+    list($file_src, $file_name, $file_hit) = explode('|', $row['file']);
+    $img_tag = '<img src="data/'.$file_src.'" width="350">';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $row['subject'] ?></title>
+</head>
+<body>
+    <table border="1" width="500">
+        <tr>
+            <td width="100">제목</td>
+            <td width="400" colspan="3"><?= $row['subject'] ?></td>
+        </tr>
+        <tr>
+            <td>조회 수</td>
+            <td width="100"><?= $row['hit'] ?></td>
+            <td width="100">등록일시</td>
+            <td width="200"><?= $row['rdatetime'] ?></td>
+        </tr>
+        <tr>
+            <td colspan="4" style="padding:20px;">
+                <?= nl2br($row['content']) . $img_tag ?>
+            </td>
+        </tr>
+        <?php if($row['file']) { ?>
+        <tr>
+            <td>파일</td>
+            <td colspan="3"><a href="download.php?idx=<?= $row['idx'] ?>"><?= $file_name ?></a> (다운로드 <?= $file_hit ?> 회)</td>
+        </tr>
+        <?php            
+        }
+        ?>
+    </table>    
+</body>
+</html>
