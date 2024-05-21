d69632ffecf5b4a79c42ad9cc1fc0d1f9ee99d63
kingchobo <kingchobo2022@gmail.com>
2024-05-22 06:53:40 +0900

    25.5.22 write.php form check

--- app/public/write.php
+++ app/public/write.php
@@ -20,8 +20,31 @@
 
 <script>
     const btn_submit = document.getElementById('btn_submit')
+    const f = document.write_form
     btn_submit.addEventListener("click", function(){
-        alert(1);
+        
+        if (f.name.value == '') {
+            alert('이름을 입력해 주세요.')
+            f.name.focus()
+            return
+        }
+        if (f.passwd.value == '') {
+            alert('암호를 입력해 주세요.')
+            f.passwd.focus()
+            return
+        }
+        if (f.subject.value == '') {
+            alert('제목을 입력해 주세요.')
+            f.subject.focus()
+            return
+        }
+        if (f.content.value == '') {
+            alert('내용을 입력해 주세요.')
+            f.content.focus()
+            return
+        }
+
+        f.submit()        
     })
 </script> 
 
